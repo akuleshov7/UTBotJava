38224eea3b7d264dd596e6ddd7a199a6228b80dc
Sergey Pospelov <sergeypospelov59@gmail.com>
2022-06-23 14:00:27 +0300

    Remove isInNestedMethod function from engine

--- utbot-framework/src/main/kotlin/org/utbot/engine/Traverser.kt
+++ utbot-framework/src/main/kotlin/org/utbot/engine/Traverser.kt
@@ -1306,7 +1306,7 @@ class Traverser(
      * Returns null if mock is not allowed - Engine traverses nested method call or parameter type is not RefType.
      */
     private fun parameterMockInfoGenerator(parameterRef: IdentityRef): UtMockInfoGenerator? {
-        if (isInNestedMethod()) return null
+        if (environment.state.isInNestedMethod()) return null
         if (parameterRef !is ParameterRef) return null
         val type = parameterRef.type
         if (type !is RefType) return null
@@ -1518,7 +1518,7 @@ class Traverser(
     }
 
     private suspend fun FlowCollector<UtResult>.traverseThrowStmt(current: JThrowStmt) {
-        val symException = explicitThrown(resolve(current.op), isInNestedMethod())
+        val symException = explicitThrown(resolve(current.op), environment.state.isInNestedMethod())
         traverseException(current, symException)
     }
 
@@ -3540,7 +3540,7 @@ class Traverser(
     ) {
         if (environment.state.executionStack.last().doesntThrow) return
 
-        val symException = implicitThrown(exception, findNewAddr(), isInNestedMethod())
+        val symException = implicitThrown(exception, findNewAddr(), environment.state.isInNestedMethod())
         if (!traverseCatchBlock(environment.state.stmt, symException, conditions)) {
             environment.state.expectUndefined()
             val nextState = environment.state.createExceptionState(
@@ -3667,7 +3667,9 @@ class Traverser(
 
             workaround(REMOVE_ANONYMOUS_CLASSES) {
                 val sootClass = returnValue.type.sootClass
-                if (!isInNestedMethod() && (sootClass.isAnonymous || sootClass.isArtificialEntity)) return
+                if (!environment.state.isInNestedMethod() && (sootClass.isAnonymous || sootClass.isArtificialEntity)) {
+                    return
+                }
             }
         }
 
@@ -3688,7 +3690,7 @@ class Traverser(
         val updatedMemory = memory.update(queuedSymbolicStateUpdates.memoryUpdates)
 
         //no need to respect soft constraints in NestedMethod
-        val holder = newSolver.check(respectSoft = !isInNestedMethod())
+        val holder = newSolver.check(respectSoft = !environment.state.isInNestedMethod())
 
         if (holder !is UtSolverStatusSAT) {
             logger.trace { "processResult<${environment.method.signature}> UNSAT" }
@@ -3696,7 +3698,7 @@ class Traverser(
         }
 
         //execution frame from level 2 or above
-        if (isInNestedMethod()) {
+        if (environment.state.isInNestedMethod()) {
             // static fields substitution
             // TODO: JIRA:1610 -- better way of working with statics
             val updates = if (environment.method.name == STATIC_INITIALIZER && substituteStaticsWithSymbolicVariable) {
@@ -3792,8 +3794,6 @@ class Traverser(
             emitFailedConcreteExecutionResult(stateBefore, e)
         }
     }
-
-    internal fun isInNestedMethod() = environment.state.isInNestedMethod()
 
     private fun ReturnStmt.symbolicSuccess(): SymbolicSuccess {
         val type = environment.method.returnType
